dcb0081915dc16b21e1953d6c789ffd47cd83e94
Albert Torres <albrewot@gmail.com>
2019-12-02 10:35:01 -0400

    Updates

---
+++ precache-manifest.4bf95d409d1f8d03a4b26ef85a702e1d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86456f1fb1ca28eac28988704cb26a90",
+    "url": "/react-roguelike/index.html"
+  },
+  {
+    "revision": "2bf320c23bdc98c9a287",
+    "url": "/react-roguelike/static/js/2.fe5305e9.chunk.js"
+  },
+  {
+    "revision": "6ad1b760d9e4df93bcf5",
+    "url": "/react-roguelike/static/js/main.9e524d5a.chunk.js"
+  },
+  {
+    "revision": "e2ea57d295e8c8e1553a",
+    "url": "/react-roguelike/static/js/runtime-main.8ae70fea.js"
+  }
+]);
